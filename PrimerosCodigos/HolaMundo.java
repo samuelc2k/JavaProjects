e13a22df8d0882aff472be25723f3e97181ab619
samuelc2k <142862402+samuelc2k@users.noreply.github.com>
2023-08-22 17:14:52 -0500

    Add files via upload

---
+++ PrimerosCodigos/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package holamundo;
+
+import static java.time.Clock.system;
+import static java.time.InstantSource.system;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo ;)");
+    }
+    
+}
