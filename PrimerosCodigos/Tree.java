61f4c46358d920a742c7e7fc38225ef15a824ef8
samuelc2k <142862402+samuelc2k@users.noreply.github.com>
2023-09-05 16:42:03 -0500

    Create Tree.java

---
+++ PrimerosCodigos/Tree.java
@@ -0,0 +1,19 @@
+    class Tree
+    {
+
+    public static void main(String[] args) {
+        System.out.println("Hey There, I'm Samu and I'm learning to code in Java!");
+        System.out.println(" I'm going to plan a tree today!" or "Ready to get my hands dirty!");
+        System.out.println("   *   ");
+        System.out.println("  ***  ");
+        System.out.println("*******");
+        System.out.println(" ***** ");
+        System.out.println("  ***  ");
+        System.out.println("  ||  ");
+        System.out.println("  ||  ");
+        System.out.println("  ||  ");
+        System.out.println("  ||  ");
+    }
+    }
+    
+}
