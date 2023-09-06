ccd678c16aeb57996df4c6f0581ca66a2ca9c2e4
samuelc2k <142862402+samuelc2k@users.noreply.github.com>
2023-09-06 13:07:48 -0500

    Create ConditionalExamples.java

---
+++ Conditionals/ConditionalExamples.java
@@ -0,0 +1,19 @@
+package conditionalexamples;
+
+public class ConditionalExamples {
+    
+    public static void main(String[] arg){
+        //All code application logic here
+        
+        int x = 10;
+        int y = 12;
+        
+        if(x+y > 20){
+            System.out.println("x + y is greater than 20");
+            
+        }
+                
+        
+    }
+    
+}
