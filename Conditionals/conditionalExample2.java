e1fce0ab0032c7bab0f5cec31b272dbdc1bdd92e
samuelc2k <142862402+samuelc2k@users.noreply.github.com>
2023-09-06 13:12:02 -0500

    Create conditionalExample2.java

---
+++ Conditionals/conditionalExample2.java
@@ -0,0 +1,21 @@
+package conditionalexamples;
+
+public class ConditionalExample2 {
+    
+    public static void main(String[] arg){
+        //All code application logic here
+        
+        int x = 10;
+        int y = 12;
+        
+        if(x+y < 10){
+            System.out.println("x + y is less than 10");
+            
+        } else {
+            System.out.println("x + y is greater than 10");
+        }
+                
+        
+    }
+    
+}
