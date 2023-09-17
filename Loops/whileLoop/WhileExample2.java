985fcd42e20dba8826aeadbb28dbe2d48e000b69
samuelc2k <142862402+samuelc2k@users.noreply.github.com>
2023-09-17 18:07:43 -0500

    Add files via upload

---
+++ Loops/whileLoop/WhileExample2.java
@@ -0,0 +1,14 @@
+
+package whileexample2;
+
+public class WhileExample2 {
+
+    public static void main(String[] args) {
+        // setting the infinite while loop by passing true to the condition
+        while(true){
+            System.out.println("infinitive while loop");
+        }
+        
+    }
+    
+}
