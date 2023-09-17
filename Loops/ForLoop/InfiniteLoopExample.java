14b3653579b7c205418936e96db6966415cf31de
samuelc2k <142862402+samuelc2k@users.noreply.github.com>
2023-09-17 17:29:01 -0500

    Add files via upload

---
+++ Loops/ForLoop/InfiniteLoopExample.java
@@ -0,0 +1,12 @@
+
+package infiniteloopexample;
+
+public class InfiniteLoopExample {
+
+    public static void main(String[] args) {
+        for(;;){
+            System.out.println("infinitive loop");
+        }
+    }
+    
+}
