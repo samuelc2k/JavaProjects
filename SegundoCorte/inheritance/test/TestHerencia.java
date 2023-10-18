a5fca38012252a5cbe739dcd647861e2e92b985d
Samuel Carrera <142862402+samuelc2k@users.noreply.github.com>
2023-10-18 13:56:32 -0500

    Create TestHerencia.java

---
+++ SegundoCorte/inheritance/test/TestHerencia.java
@@ -0,0 +1,17 @@
+package test;
+
+import herencia.*;
+
+import java.util.Date;
+public class TestHerencia {
+    
+    public static void main(String[] args){
+        
+        Employee empleado1  = new Employee("Alex", 3000);
+        System.out.println(empleado1);
+        Employee empleado2 = new Employee("Reol", 4000);
+        System.out.println(empleado2);
+    
+    }
+    
+}
